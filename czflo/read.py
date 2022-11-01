c3b647e6263c4f4e1e9c4d27840c4a67183de279
klimarichard <klima.risa@gmail.com>
2022-11-01 17:07:25 +0100

    Creating Python package for frequently used functions.

---
+++ czflo/read.py
@@ -0,0 +1,29 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+def read_league_table_old(tbl: str):
+    """
+    Reads league team stats.
+    !! WARNING !! This function is deprecated since October 2022 when
+    Český florbal released a new website.
+    :param tbl: URL with table location
+    :return: nothing
+    """
+    from czflo.write import write_table_to_file_old
+
+    url = tbl
+    page = requests.get(url)
+
+    # if opening the page was OK
+    if page.status_code == 200:
+        print("League table page opened successfully...")
+
+        soup = BeautifulSoup(page.text, 'html.parser')
+
+        table = soup.find('tbody', id=re.compile('^datablock-TeamStatsDataGrid_'))
+
+        write_table_to_file_old(table, f'aktualni/csv/league_table.csv')
+    else:
+        print(f'ERROR: Unsuccessful loading of league table page!')
---
+++ czflo/write.py
@@ -0,0 +1,19 @@
+import bs4
+
+
+def write_table_to_file_old(table: bs4.BeautifulSoup, file_name: str):
+    """
+    Writes a parsed table to a given CSV file.
+    :param table: a BeautifulSoup element with the table body
+    :param file_name: path to target CSV file
+    :return: nothing
+    """
+    with open(file_name, 'w', encoding='utf-8') as f:
+        for tr in table.find_all('tr'):
+            s = ''
+            for td in tr.find_all('td'):
+                s += td.text.strip()
+                s += ','
+
+            s = s[:-1] + '\n'
+            f.write(s)
